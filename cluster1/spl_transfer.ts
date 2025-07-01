a6489c5e2a90b3e8b74c0933957b60e2d41f1b40
0xDev05 <divpatel1274@gmail.com>
2025-07-01 21:22:53 +0530

    spl_transfer done

--- cluster1/spl_transfer.ts
+++ cluster1/spl_transfer.ts
@@ -10,18 +10,22 @@ const commitment: Commitment = "confirmed";
 const connection = new Connection("https://api.devnet.solana.com", commitment);
 
 // Mint address
-const mint = new PublicKey("<mint address>");
+const mint = new PublicKey("7h9C95AzCVPomPJYSHN13BVEZYbvHCW981TWsx31udph");
 
 // Recipient address
-const to = new PublicKey("<receiver address>");
+const to = new PublicKey("DZwXmjwngoyTj6JiaaJ1R5bNibouEoHtoF6jU4GBe788");
 
 (async () => {
     try {
         // Get the token account of the fromWallet address, and if it does not exist, create it
+        const fromWallet = await  getOrCreateAssociatedTokenAccount(connection,keypair,mint,keypair.publicKey,);
 
         // Get the token account of the toWallet address, and if it does not exist, create it
+        const toWallet = await getOrCreateAssociatedTokenAccount(connection,keypair,mint,to);
 
         // Transfer the new token to the "toTokenAccount" we just created
+        const singnature = await transfer(connection,keypair,fromWallet.address,toWallet.address,keypair.publicKey,7n*1_000_000n);
+        console.log("tranasction singnature: https://explorer.solana.com/tx/{}?cluster=devnet",singnature);
     } catch(e) {
         console.error(`Oops, something went wrong: ${e}`)
     }
